3316b719aea7915b11dc1053d7bb45676e6b461b
Xianming Zhong <chinesedfan@qq.com>
2019-10-13 23:25:56 +0800

    render in group, including diff bar and value

--- src/components/bar-chart.vue
+++ src/components/bar-chart.vue
@@ -4,6 +4,10 @@
 <script>
 import { CarProps } from '../constants/game';
 
+function getNumberAttr(selection, name) {
+    return parseInt(selection.attr(name));
+}
+
 export default {
     props: ['car', 'otherCar'],
     watch: {
@@ -29,41 +33,52 @@ export default {
                 .attr('width', w)
                 .attr('height', h);
 
-            // labels
-            const labelWidth = 70;
-            svg.selectAll('text')
+            const self = this;
+            svg.selectAll('g')
                 .data(CarProps)
                 .enter()
-                .append('text')
-                .attr('x', 0)
-                .attr('y', (d, i) => i * 30 + 18)
-                .text((d) => d);
+                .append('g')
+                .each(function(d, i) {
+                    const labelWidth = 70;
+                    const barWidth = 330;
+                    const g = d3.select(this);
 
-            // bars
-            const barWidth = 330;
-            svg.selectAll('rect')
-                .data((this.otherCar || this.car).properties)
-                .enter()
-                .append('rect')
-                .attr('fill', 'blue')
-                .attr('x', labelWidth + 20)
-                .attr('y', (d, i) => i * 30)
-                .attr('width', (d) => Math.floor(d * barWidth / 1000))
-                .attr('height', 25);
+                    const car = self.otherCar || self.car;
+                    const diff = car.properties[i] - self.car.properties[i];
 
-            // diffs
-            svg.append('g')
-                .selectAll('text')
-                .data(this.car.properties)
-                .enter()
-                .append('text')
-                .attr('x', labelWidth + 20 + barWidth + 20)
-                .attr('y', (d, i) => i * 30 + 18)
-                .text((d, i) => {
-                    if (!this.otherCar) return d;
+                    // label
+                    const sLabel = g.append('text')
+                        .attr('x', 0)
+                        .attr('y', i * 30 + 18)
+                        .text(d);
+                    // bar
+                    const sBar = g.append('rect')
+                        .attr('fill', 'blue')
+                        .attr('x', labelWidth + 20)
+                        .attr('y', i * 30)
+                        .attr('width', Math.floor(car.properties[i] * barWidth / 1000))
+                        .attr('height', 25);
+                    // value
+                    const sValue = g.append('text')
+                        .attr('x', labelWidth + 20 + barWidth + 20)
+                        .attr('y', sLabel.attr('y'))
+                        .text(Math.min(car.properties[i], self.car.properties[i]));
 
-                    const diff = this.otherCar.properties[i] - d;
-                    return `${d} (${diff < 0 ? diff : '+' + diff})`;
+                    if (!self.otherCar) return;
+                    // diff bar
+                    const diffColor = diff < 0 ? 'green' : 'red';
+                    g.append('rect')
+                        .attr('fill', diffColor)
+                        .attr('x', getNumberAttr(sBar, 'x') + getNumberAttr(sBar, 'width'))
+                        .attr('y', i * 30)
+                        .attr('width', Math.floor(Math.abs(diff) * barWidth / 1000))
+                        .attr('height', 25);
+                    // diff value
+                    diff && g.append('text')
+                        .attr('fill', diffColor)
+                        .attr('x', getNumberAttr(sValue, 'x') + 30)
+                        .attr('y', sValue.attr('y'))
+                        .text(diff < 0 ? `(${diff})` : `(+${diff})`);
                 });
         }
     },
